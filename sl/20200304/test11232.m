78333ff752a75e6919a65c6babc9fdf4332eb7de
a4327481 --global user.name a4327481 <a4327481@126.com>
2020-08-26 11:22:13 +0800

    day 20200826 01

--- sl/20200304/test11232.m
+++
@@ -1,6 +0,0 @@
-
-        switch PathModeOut.flightTaskMode                 
-         case    {ENUM_FlightTaskMode.TakeOffMode,ENUM_FlightTaskMode.LandMode} 
-        2
-                
-        end
